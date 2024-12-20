2f1801b156a5651be7d0a715d9c56e6b67d2c879
Pavan Singh <psinghp888@gmail.com>
2024-12-21 02:03:17 +0530

    tailwind + Daisy UI + Routing added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
-
+import Body from "./components/Body";
+import { BrowserRouter,Routes,Route } from "react-router-dom";
+import Login from "./components/Login";
+import Profile from "./components/Profile";
+import Connections from "./components/Connections";
 
 function App() {
 
   return (
     <>
-      <h1>Hello</h1>
-    </>
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Body/>}>
+       <Route path="/" ></Route>
+       <Route  path='/login' element={<Login/>}></Route>
+       <Route  path='/profile' element={<Profile/>}></Route>
+       <Route  path='/connections' element={<Connections/>}></Route>
+      </Route>
+    </Routes> 
+    </BrowserRouter>
+</>
   )
 }
 
